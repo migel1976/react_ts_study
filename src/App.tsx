7f37df493246693f862521beafdc36add82a28c0
smirnov <smirnov@alfasatcom.ru>
2022-11-20 13:53:06 +0300

    card components which are shown how use props in ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import Card from './components/Card'
+import React from 'react'
+import Card, {CardVariant} from './components/Card'
 
 const App=()=>{
+	const click=(num:number)=>{
+				alert('you just now click on Card component '+num)
+	}
 	return (
 		<div>
 			<h1>
 				hello world
 			</h1>
-			<Card width='200px' height='300px' />
+			<Card 
+				onClick={click}
+				width='200px'
+				height='300px'
+				variant={CardVariant.primary}
+			>
+				<button>click me</button>
+			</Card>
 		</div>
 	)
 
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, {FC, useState} from 'react'
+
+export enum CardVariant{
+	outlined='outlined',
+	primary='primary'
+}
 
 interface CardProps{
 	width:string,
 	height:string,
+	children:React.ReactNode,
+	variant?:CardVariant,
+	onClick:(num:number)=>void
 }
 
-const Card=({width, height}:CardProps)=>{
+const Card:FC<CardProps>=({
+	width,
+	height,
+	children,
+	variant,
+	onClick
+	})=>{
+	const [state, setState]=useState(46)
 	return(
-		<div style={{width, height, border:'1px solid gray'}}>
-			aloha from card
+		<div style={{
+			width,
+			height,
+			border:variant===CardVariant.outlined ? '1px solid gray' : 'none',
+			background:variant===CardVariant.primary ? 'green' :''
+			}}
+			onClick={()=>onClick(state)}
+		>
+			{children}
 		</div>
 	)
 }
